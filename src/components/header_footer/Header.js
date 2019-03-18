35751ffff6b07fe86f79139c0d15bcaab84e69f4
Darryl <darrylmangibin@gmail.com>
2019-03-18 13:34:15 +0800

    [UDPATE]-header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -52,7 +52,7 @@ class Header extends Component {
 
                     <div className="header_logo">
                         <div className="font_righteous header_logo_venue">Darryl</div>
-                        <div className="header_logo_title">Rockstart Web Developer</div>
+                        <div className="header_logo_title">Rockstar Web Developer</div>
                     </div>
 
                     <IconButton
