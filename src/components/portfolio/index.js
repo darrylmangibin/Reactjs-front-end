90cc7c58d138730880168a4c945ab62d623cedf0
Darryl Mangibin <darrylmangibin@gmail.com>
2020-05-13 12:31:34 +0800

    Update portfolio items

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -9,29 +9,29 @@ class portfolio extends Component {
 			"React",
 			"React",
 			"React",
-            "CSS",
-            "Ruby on rails",
-			"Front-End",
-			"Front-End",
-			"Front-End",
-			"Front-End",
+			"CSS",
+			"Ruby on rails",
 			"Github",
+			"Front-End",
+			"Front-End",
+			"Front-End",
+			"Front-End",
 		],
 		items: [
 			"Expensify",
 			"Social Media for developers",
 			"Pure React Javascript",
 			"Guitar Website",
-            "CSS and SASS with animations",
-            "Ruby on rails(CRUD APP)",
+			"CSS and SASS with animations",
+			"Ruby on rails(CRUD APP)",
+			"Github Repository",
 			"Asphalt-legends",
 			"Disney princess majestic quest",
 			"Getawayblast",
 			"Asphalt eSports",
 			"Minion rush",
-            "Lego legacy",
-            "Over Drive City",
-			"Github Repository",
+			"Lego legacy",
+			"Over Drive City",
 		],
 		desc: [
 			"CRUD Application for your expenses. You can list down all your expenses. Back end - Firebase",
@@ -42,41 +42,41 @@ class portfolio extends Component {
 
 			"Ecom site - Have an Admin use that can create his/her items to sell and can edit items. Back end - Node(express) MongoDB",
 
-            "A Pure Css with Sass. Animation are all css no javascript involve",
-            
-            "Application that deals crup operation(Simple, take not that I didn't have styling. The aim is to show Rails functionality)",
+			"A Pure Css with Sass. Animation are all css no javascript involve",
 
-			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-
-			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-
-			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-
-			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-
-			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-
-            "These are some of the projects in Gameloft and I am part of building as a Front-end developer",
-            
-            "These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+			"Application that deals crup operation(Simple, take not that I didn't have styling. The aim is to show Rails functionality)",
 
 			"You can view my activities or source code of all my projects in my Github",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
+
+			"These are some of the projects in Gameloft and I am part of building as a Front-end developer",
 		],
 		linkto: [
 			"https://fierce-thicket-60455.herokuapp.com/",
 			"https://vivixx-dev.herokuapp.com/profile/darryl",
 			"http://inquisitive-pollution.surge.sh/",
 			"https://nameless-ocean-14676.herokuapp.com/",
-            "https://darrylmangibin.github.io/CSS-SASS-/",
-            "https://fathomless-bayou-60436.herokuapp.com/",
+			"https://darrylmangibin.github.io/CSS-SASS-/",
+			"https://fathomless-bayou-60436.herokuapp.com/",
+			"https://github.com/darrylmangibin",
 			"https://www.asphaltlegends.com/",
 			"https://www.gameloft.com/disneyprincess-majesticquest/en/",
 			"http://www.gameloft.com/en/game/disneygetawayblast/",
 			"https://www.asphaltesports.com/en/",
-            "https://minionrush.com/",
-            "https://www.legolegacy.com/",
-            "https://www.gameloft.com/en/game/overdrive-city",
-			"https://github.com/darrylmangibin",
+			"https://minionrush.com/",
+			"https://www.legolegacy.com/",
+			"https://www.gameloft.com/en/game/overdrive-city",
 		],
 		sourceCode: [
 			"https://github.com/darrylmangibin/React-expensify",
@@ -84,7 +84,8 @@ class portfolio extends Component {
 			"https://github.com/darrylmangibin/Reactjs-indecsion-app-v2",
 			"https://github.com/darrylmangibin/waves",
 			"https://github.com/darrylmangibin/CSS-SASS-",
-			"https://github.com/darrylmangibin/Reactjs-front-end",
+			"https://github.com/darrylmangibin/recipe",
+			"https://github.com/darrylmangibin/",
 		],
 		delay: [500, 0, 500, 500, 500],
 	};
